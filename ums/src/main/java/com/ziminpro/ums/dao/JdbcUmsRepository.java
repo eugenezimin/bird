44d18bd72b742b5e82b662d829f685806fc60947
Eugene Zimin <eugene@ziminpro.com>
2026-04-14 11:24:04 +0300

    updated build tools

--- ums/src/main/java/com/ziminpro/ums/dao/JdbcUmsRepository.java
+++ ums/src/main/java/com/ziminpro/ums/dao/JdbcUmsRepository.java
@@ -9,8 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
